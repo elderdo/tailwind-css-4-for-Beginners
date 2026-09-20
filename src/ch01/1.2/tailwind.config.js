bc5e1a8367c656e79dd7e4ddda5f33b7dcee0f56
elderdo <elderdo@yahoo.com>
2026-09-20 13:36:06 -0500

    feat: add Tailwind CSS setup and configuration
    - Updated package.json to include Tailwind CSS, PostCSS, and Auto-prefixer as dev dependencies.
    - Created a .gitignore file to exclude unnecessary files and directories from version control.
    - Added tailwind.config.js for Tailwind CSS configuration with custom colors.
    - Created ultra.dark.html to demonstrate dark mode styling using Tailwind CSS.
    - Added styles.css to include Tailwind's base, components, and utilities styles.

---
+++ src/ch01/1.2/tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  darkmode: "class", // switch by adding dark on <html> or <body>
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          950: "#0f172a", // new ultra-dark shade
+          50: "#fof9ff",
+        },
+      },
+    },
+  },
+};
